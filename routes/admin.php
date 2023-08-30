145c95240c383bb33ea654ef27475cf0fa47f41f
AkramBarakat <akramabarakat.2003@gmail.com>
2023-08-30 21:06:06 +0300

    route

---
+++ routes/admin.php
@@ -0,0 +1,15 @@
+<?php
+Route::get('admin/home',function(){
+    return 'admin home';
+});
+Route::get('admin/proudect',function(){
+    return 'admin proudect';
+});
+Route::get('admin/orders',function(){
+    return 'admin orders';
+});
+Route::get('admin/seles',function(){
+    return 'admin seles';
+});
+
+
--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return "home page";
 });
 
-
-Route::get('/about', function () {
-    return view('welcome');
+Route::get('/about', function(){
+    return 'About Page';  
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/contact', function(){
+    return 'contact page';
+});
